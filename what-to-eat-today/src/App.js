8ca502788f7a82fb472828b216e97b6450f7884d
annaleee <annaleee@umich.edu>
2023-01-17 01:28:00 -0500

    SD

--- what-to-eat-today/src/App.js
+++ what-to-eat-today/src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{ useState } from 'react';
 
 function App() {
+  const menu = ['热干面拌一切','泡菜饼','有啥拌啥饭','有啥丢啥汤面','有啥丢啥味增汤']
+  const rest = ['China Palace','老地方','No Thai','碧都','Lan city','One Bowl']
+  const [result,setResult] = useState('');
+
+  function generateRest(){
+    setResult(rest[Math.floor(Math.random()*menu.length)])
+  }
+  function generateMenu(){
+    setResult(menu[Math.floor(Math.random()*menu.length)])
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p id="ResultArea">{result}</p>
+      <div id="ButtonArea">
+        <button className="Button" onClick={()=>generateRest()}> 
+          <p>饭店</p>
+        </button>
+        <button className='Button' onClick={()=>generateMenu()}> 
+          <p>菜谱</p>
+        </button>
+      </div>
     </div>
   );
 }
